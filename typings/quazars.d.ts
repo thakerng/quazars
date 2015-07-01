5d0c3a58c56e366784020c7c2760e2860d47a649
thakerng <southbridge_855@hotmail.com>
2015-07-01 21:38:27 +0700

    add gititnore

---
+++ typings/quazars.d.ts
@@ -0,0 +1,23 @@
+///<reference path='express.d.ts'/>
+declare var quazars:any;
+
+interface IConfiguration{
+    name:string;
+    basePath?:string;
+    appPath?:string;
+    quazarsPath?:string;
+    author?:{
+        name?:string;
+        email?:string;
+        company?:string;
+    };
+    modules?:Array<string>;
+    appConfig?:string;
+    preload?:Array<string>;
+}
+
+interface  Provider{
+    register():void;
+    boot():void;
+    build?():void;
+}
