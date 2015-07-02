8477e43f2e3e4f54a9803b1a2edb0ee36265443c
thakerng <southbridge_855@hotmail.com>
2015-07-02 22:05:38 +0700

    Add Decorator

---
+++ Class/Decorator.ts
@@ -0,0 +1,25 @@
+
+function timeStamp(...input:any[]){
+    var order=0,times = input;
+    if(typeof input[0] === 'number') {
+        order = input[0];
+        input.shift();
+        times = input;
+    }
+
+    return (target:Object,propertyKey:string,descriptor:any) => {
+        var former = descriptor.value;
+        descriptor.value = function(...args:any[]){
+            for(var time of times) {
+                if (time == 'created')
+                    args[order].createdAt = new Date();
+                else if(time =='updated')
+                    args[order].updatedAt = new Date();
+            }
+            former.apply(this,args);
+        };
+        return descriptor;
+    }
+}
+
+export {timeStamp};
