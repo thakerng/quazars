3e129c7ea84efed0dafda67cc74535edc6151994
thakerng <southbridge_855@hotmail.com>
2015-07-24 20:19:03 +0700

    Add Session

--- Kernel.ts
+++ Kernel.ts
@@ -33,6 +33,7 @@ class Kernel{
         this.app.use(logger('dev'));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(express.session({ secret: 'quazars' }));
         this.app.use(cookieParser());
         global.quazars.path = this._config.quazarsPath;
         global.quazars.kernel = path.resolve(this._config.quazarsPath,'Kernel');
